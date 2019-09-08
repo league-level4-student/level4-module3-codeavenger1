5803160dadf8e0c8d1021040f8187ce326c36c03
League Student <league@iMac-14.attlocal.net>
2019-09-08 16:52:01 -0700

    finished mod 3

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,8 +1,22 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	public void divide(){
+	public static double divide(double a, double b) throws IllegalArgumentException {
+		if (b == 0.0) {
+			throw new IllegalArgumentException();
+		}
+		return a/b;
+	}
+	
+	public static String reverseString(String s) throws IllegalStateException {
+		String reverse = "";
 		
-		
+		if(s.equals("")) {
+			throw new IllegalStateException();
+		}
+		for (int i = s.length(); i < 0; i--) {
+			reverse += s.charAt(i);
+		}
+		return reverse;
 	}
 }
--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -32,7 +32,11 @@ class ExceptionsTest {
 	//3. Complete the JUnit test method to test the divide method.
 	@Test
 	public void testDivideByZeroException() {
-		
+		try {
+			ExceptionMethods.divide(5.5, 5.5);
+		} catch(Exception e){
+			System.out.println("hi");
+		}
 	}
 	
 	//4. In the ExceptionMethods class, write a method called reverseString that takes a
@@ -42,7 +46,11 @@ class ExceptionsTest {
 	//5. Complete the JUnit test method to test the reverseStringMethod.
 	@Test
 	public void testReverseString() {
-		
+		try {
+			ExceptionMethods.reverseString("");
+		} catch(Exception e) {
+			System.out.println("hello");
+		}
 	}
 	
 	
